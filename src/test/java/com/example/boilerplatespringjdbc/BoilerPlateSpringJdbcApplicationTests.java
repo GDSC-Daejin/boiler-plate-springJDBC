7c7768b589c9c44f3c76326d1b6b6bf9fbd2991c
gudcks0305 <gudcks0305>
2022-07-08 06:10:11 +0900

    first commit

---
+++ src/test/java/com/example/boilerplatespringjdbc/BoilerPlateSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boilerplatespringjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerPlateSpringJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
